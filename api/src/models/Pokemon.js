b22544b6b600b81f6cbb5ab8dfbf6532dd9ca076
Kary1661 <posposkarina@gmail.com>
2023-06-12 18:16:18 -0300

    'changes'

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
@@ -31,15 +32,17 @@ module.exports = (sequelize) => {
     },
     speed: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
     height: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
     weight: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
+    }, {
+     timestamps: false
   });
 };
--- api/src/models/Type.js
+++ api/src/models/Type.js
@@ -3,13 +3,16 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('type', {
         id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
+            type: DataTypes.INTEGER,
             primaryKey: true,
+            autoIncrement: true,
         },
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
         },
+    }, {
+        timestamps: false,
     });
     }
