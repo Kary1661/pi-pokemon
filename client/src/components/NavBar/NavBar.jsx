4defb7296d72c7c26162b12934c8906e6bdf7df9
Kary1661 <posposkarina@gmail.com>
2023-06-19 03:00:40 -0300

    'lm'

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     return(
         <div className={style.mainContainer}>
             <nav className={style.navbar}>
-                <div className={style.logo}><NavLink to="/home"><img src={pokeLogo} alt="pokeLogo"></img></NavLink></div>
+                <div className={style.logo}><NavLink to="/home"><img src={pokeLogo} alt="logo"></img></NavLink></div>
                 <div className={style.navLinks}>
                     <NavLink to="/create" className={style.link}>Create Pokemon</NavLink>
                     <NavLink to="/" className={style.link}>Logout</NavLink>
